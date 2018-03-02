913165f7a7f60aa42060b7e8c6c3f330edd944f4
Hermann Harris <harris.hermann@gmail.com>
2018-03-02 10:30:06 +0200

    Added conversion functions

--- README.md
+++ README.md
@@ -5,24 +5,35 @@ Convert integers and string formatted integers to a properly formatted currency 
 
 [![Build Status](https://travis-ci.org/HermannHH/mcduck.svg?branch=master)](https://travis-ci.org/HermannHH/mcduck)
 
+[![Coverage Status](https://coveralls.io/repos/github/HermannHH/mcduck/badge.svg?branch=master)](https://coveralls.io/github/HermannHH/mcduck?branch=master)
+
 ## Installation
 
-  `npm install mcduck --save` or `yarn dd mcduck --save`
+  `npm install mcduck --save` or `yarn add mcduck --save`
 
 ## Usage
 
     RequireJS:
 
-    var numFormatter = require('mcduck');
+    const mcduck = require('mcduck');
 
-    var formattedNum = mcduck(35666);
+    const formattedCurrency = mcduck.toCurrency('356660', 'USD');
+    Output should be 'USD 3 566.60';
+    const formattedCurrencyToInteger = mcduck.toInteger(formattedCurrency);
+    Output should be 356660;
+    const formattedCurrencyToIntegerString = mcduck.toIntegerString(formattedCurrency);
+    Output should be '356660';
 
     es6:
     import mcduck from 'mcduck';
 
-    const formattedNum = mcduck(35666)
-  
-  Output should be `35,666`
+    const formattedCurrency = mcduck.toCurrency('356660', 'USD');
+    Output should be 'USD 3 566.60';
+    const formattedCurrencyToInteger = mcduck.toInteger(formattedCurrency);
+    Output should be 356660;
+    const formattedCurrencyToIntegerString = mcduck.toIntegerString(formattedCurrency);
+    Output should be '356660';
+
 
 
 ## Tests
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
-var toCurrency = function(x) {
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+var toCurrency = function(x, symbol='USD') {
   let input = '0';
   if (x) {
-    input = x;
+    input = isNumber(x) ? x.toString() : x;
   }
   const noPoints = input.replace(/\D/g, '');
   const noLeadingZeros = noPoints.replace(/^[0]+/g, '');
@@ -12,8 +16,30 @@ var toCurrency = function(x) {
   const splitter = stringWithDecimal.split('.');
   const leftDecimal = splitter[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
   const rightDecimal = splitter[1];
-  return `ZAR ${leftDecimal}.${rightDecimal}`;
+  return `${symbol} ${leftDecimal}.${rightDecimal}`;
 };
-// var myFunc2 = function() { ... };
-// exports.myFunc1 = myFunc1;
+
+var toIntegerString = function() {
+  let input = '0';
+  if (x) {
+    input = isNumber(x) ? x.toString() : x;
+  } 
+  const noPoints = input.replace(/\D/g, '');
+  const noLeadingZeros = noPoints.replace(/^[0]+/g, '');
+  return noLeadingZeros.length === 0 ? '0' : noLeadingZeros;
+}
+
+var toInteger = function(x) {
+  let input = '0';
+  if (x) {
+    input = isNumber(x) ? x.toString() : x;
+  } 
+  const noPoints = input.replace(/\D/g, '');
+  const noLeadingZeros = noPoints.replace(/^[0]+/g, '');
+  return parseInt(noLeadingZeros.length === 0 ? '0' : noLeadingZeros, 10);
+}
+
+
 exports.toCurrency = toCurrency;
+exports.toIntegerString = toIntegerString;
+exports.toInteger = toInteger;
