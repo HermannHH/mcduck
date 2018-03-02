3d8b4fc15275cfb6c4381b152067d2b7640df31a
Hermann Harris <harris.hermann@gmail.com>
2018-03-02 10:49:18 +0200

    Added test for integer conversion

--- test/test.js
+++ test/test.js
@@ -4,8 +4,12 @@ var expect = require('chai').expect;
 var mcduck = require('../index');
 
 describe('#toCurrency', function() {
-    it('should convert single digits', function() {
+    it('should convert integer string to currency', function() {
         var result = mcduck.toCurrency('1', 'ZAR');
+        expect(result).to.equal('ZAR 0.01');
+    });
+    it('should convert integer to currency', function() {
+        var result = mcduck.toCurrency(1, 'ZAR');
         expect(result).to.equal('ZAR 0.01');
     });
 });
