636d700fb6a2ff6d879161f599111509295d02e7
Hermann Harris <harris.hermann@gmail.com>
2018-03-02 10:07:03 +0200

    Added initial stab at function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 'use strict';
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-
- 
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+var toCurrency = function(x) {
+  let input = '0';
+  if (x) {
+    input = x;
+  }
+  const noPoints = input.replace(/\D/g, '');
+  const noLeadingZeros = noPoints.replace(/^[0]+/g, '');
+  const workableString = noLeadingZeros.length < 3 ? `${'000'.substr(0, 3 - noLeadingZeros.replace(/^[0]+/g, '').length)}${noLeadingZeros}` : noLeadingZeros;
+  const stringWithDecimal = `${workableString.substr(0, workableString.length - 2)}.${workableString.substr(workableString.length - 2)}`;
+  const splitter = stringWithDecimal.split('.');
+  const leftDecimal = splitter[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  const rightDecimal = splitter[1];
+  return `ZAR ${leftDecimal}.${rightDecimal}`;
 };
+// var myFunc2 = function() { ... };
+// exports.myFunc1 = myFunc1;
+exports.toCurrency = toCurrency;
